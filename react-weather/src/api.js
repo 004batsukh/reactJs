4ed1979c6215984b4138ea18064a09d34ad557b0
Mirakuru27 <kisetsu27@gmail.com>
2022-10-10 16:39:22 +0800

    20min

---
+++ react-weather/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'c0e57e8fe5mshcbcc81cb03da3fep1d6230jsn87870df20eea',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+
+  export const GEO_API_URL = "https://wtf-geo-db.p.rapidapi.com/v1/geo";
+  
+ 
+ 
