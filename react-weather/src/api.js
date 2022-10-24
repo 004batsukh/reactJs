713036724cc9f319b52878d4a1045e3bfd366d8f
IaBaseDi1 <112378220+004batsukh@users.noreply.github.com>
2022-10-24 13:06:09 +0800

    duussan

--- react-weather/src/api.js
+++ react-weather/src/api.js
@@ -1,14 +1,14 @@
 export const geoApiOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'c0e57e8fe5mshcbcc81cb03da3fep1d6230jsn87870df20eea',
-      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
+  method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c0e57e8fe5mshcbcc81cb03da3fep1d6230jsn87870df20eea',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
   };
 
   export const GEO_API_URL = "https://wtf-geo-db.p.rapidapi.com/v1/geo";
   
-  export const WEATHER_API_URL ="https://api.openweathermap.org/data"
+  export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5"
   export const WEATHER_API_KEY ="fbcea0534e743149e08b73f3f7780d42"
  
  
